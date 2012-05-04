76f8ae3c2fb6d9ccc9c46a1cfac8f3186bf10944
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-05-04 02:34:25 -0700

    Started new rails 3.2.3 project
    rails new licence-finder -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LicenceFinder::Application.config.secret_token = '54dec6e791dce377c9b3a28aca520b427be2b469d3f4f4632158435a7edf0e3267fadaf94520d5dd5910116a227d6c7904bc81164de4f97a6549aea144dec46d'

9a2c6fba17b82d8d3b4ad8aa8426bf6b65d548ad
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-05-04 06:37:39 -0700

    Simplify db cleaner setup

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,12 +1,11 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean
   end
 
   config.before(:each) do
     DatabaseCleaner.start
   end
-
   config.after(:each) do
     DatabaseCleaner.clean
   end

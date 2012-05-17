467577e2532f8ba7ef9b9cbdfcedd72eee3b9ed6
Frances Berriman <francesberriman@ubuntu.localdomain>
2012-05-17 09:21:54 +0000

    Checking values

--- app/assets/javascripts/licence-finder.js
+++ app/assets/javascripts/licence-finder.js
@@ -1,4 +1,23 @@
 $(function() {
- // licence finder whizzy
+
+	$(".search-picker a").on("click", function(e){
+		console.log($(this).parent().children("span").text())
+
+		var id = $(this);
+		console.log(id);
+
+		addLicence($(this).parent().children("span").text(), id)
+		
+		e.preventDefault();
+		return false;
+	})
+
+	function addLicence(plainTextTitle, id){
+		console.log("plaintext: "+plainTextTitle+" id: "+id);
+	}
+
+	function removeLicence(id){
+
+	}
 
 });

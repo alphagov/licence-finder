7881a0159df81b5ab0fcbb6369283879a8ad4edb
Frances Berriman <fberriman@gmail.com>
2012-05-18 10:28:21 +0000

    Basic working version for sectors

--- app/assets/javascripts/licence-finder.js
+++ app/assets/javascripts/licence-finder.js
@@ -1,23 +1,58 @@
 $(function() {
 
-	$(".search-picker a").on("click", function(e){
-		console.log($(this).parent().children("span").text())
+	function swapList(elem, targetList, state){
+	
+		var id = $(elem).attr("href").split("sectors=");
+		var plainTextTitle = $(elem).parent().children("span").text();
 
-		var id = $(this);
-		console.log(id);
+		if(state == "add"){
+			var toAdd = "<li><input type='hidden' value='"+id[1]+"' name='sector_ids[]' id='sector_ids_'><span class='sector-name'>"+plainTextTitle+"</span> <a href='/licence-finder/sectors?sectors="+id[1]+"'>Remove</a></li>";
+		}
+		else{
+			var toAdd = "<li><span class='sector-name'>"+plainTextTitle+"</span><a href='/licence-finder/sectors?sectors="+id[1]+"'>Add</a></li>";
+		}
 
-		addLicence($(this).parent().children("span").text(), id)
-		
-		e.preventDefault();
-		return false;
-	})
+    var added = false;
+    var targetListItems = $(targetList+" li");
 
-	function addLicence(plainTextTitle, id){
-		console.log("plaintext: "+plainTextTitle+" id: "+id);
+
+   
+
+   	$(targetListItems).each(function(){
+  		if($(this).text() > plainTextTitle){
+  			$(toAdd).insertBefore($(this));
+  				added = true;
+  				return false;
+  		};
+  	});
+  	
+  	if(!added) $(toAdd).appendTo(targetList);
+  	$(elem).parent().remove();
+  	setupEvents();
 	}
 
-	function removeLicence(id){
 
+	function setupEvents(){
+		$(".business-sector-picked a").off("click");
+		$(".business-sector-picked a").on("click", function(){
+			swapList(this, ".search-picker", "remove")
+			return false;
+		});
+		$(".search-picker a").off("click");
+		$(".search-picker a").on("click", function(){
+			swapList(this, ".business-sector-picked ul", "add")
+			return false;
+		});
 	}
+
+	function init(){
+		if($(".business-sector-picked ul").length == 0){
+			$("<ul></ul><input type='submit' value='Next step' name='commit' class='button medium'>").insertAfter(".business-sector-picked h3");
+		}
+		setupEvents();
+	}
+
+
+	init();
 
 });

d5a0ebef45b66c4ea518a04818663042b7df8d6b
Frances Berriman <fberriman@gmail.com>
2012-05-18 11:03:56 +0000

    Can go back and change first answer

--- app/assets/javascripts/licence-finder.js
+++ app/assets/javascripts/licence-finder.js
@@ -1,22 +1,29 @@
 $(function() {
 
+	/* Add one thing to another thing, alphabetically */
 	function swapList(elem, targetList, state){
 	
 		var id = $(elem).attr("href").split("sectors=");
+		if(id[1].indexOf("_")){
+			var id = id[1].split("_");
+			var id = id[0]
+		}
+		else{
+			id = id[1];
+		}
+
+		
 		var plainTextTitle = $(elem).parent().children("span").text();
 
 		if(state == "add"){
-			var toAdd = "<li><input type='hidden' value='"+id[1]+"' name='sector_ids[]' id='sector_ids_'><span class='sector-name'>"+plainTextTitle+"</span> <a href='/licence-finder/sectors?sectors="+id[1]+"'>Remove</a></li>";
+			var toAdd = "<li><input type='hidden' value='"+id+"' name='sector_ids[]' id='sector_ids_'><span class='sector-name'>"+plainTextTitle+"</span> <a href='/licence-finder/sectors?sectors="+id+"'>Remove</a></li>";
 		}
 		else{
-			var toAdd = "<li><span class='sector-name'>"+plainTextTitle+"</span><a href='/licence-finder/sectors?sectors="+id[1]+"'>Add</a></li>";
-		}
+			var toAdd = "<li><span class='sector-name'>"+plainTextTitle+"</span><a href='/licence-finder/sectors?sectors="+id+"'>Add</a></li>";
+		};
 
     var added = false;
     var targetListItems = $(targetList+" li");
-
-
-   
 
    	$(targetListItems).each(function(){
   		if($(this).text() > plainTextTitle){
@@ -29,7 +36,7 @@ $(function() {
   	if(!added) $(toAdd).appendTo(targetList);
   	$(elem).parent().remove();
   	setupEvents();
-	}
+	};
 
 
 	function setupEvents(){
@@ -43,15 +50,24 @@ $(function() {
 			swapList(this, ".business-sector-picked ul", "add")
 			return false;
 		});
-	}
+	};
 
 	function init(){
 		if($(".business-sector-picked ul").length == 0){
 			$("<ul></ul><input type='submit' value='Next step' name='commit' class='button medium'>").insertAfter(".business-sector-picked h3");
-		}
+		};
 		setupEvents();
-	}
+	};
 
+	// setup for given step section
+	function setupElements(){
+
+	};
+
+	// manage url
+	function manageURL(){
+
+	};
 
 	init();
 

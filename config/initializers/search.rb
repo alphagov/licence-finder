ef03d554583cb20e008b6eb03fc6f1f60257e25a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-05-30 13:54:13 +0100

    Add extra terms and synonyms to search.

--- config/initializers/search.rb
+++ config/initializers/search.rb
@@ -7,13 +7,14 @@ $search = lambda {
   search_config = load_config('search.yml')
   client_name   = search_config[Rails.env][:client]
   client_config = load_config("#{client_name}.yml")
+  client_config = client_config[Rails.env].merge(client_config[:all_envs] || {})
   if client_name.to_sym == :elasticsearch
     Rails.logger.instance_eval do
       alias :write :info
     end
-    client = Search::Client::Elasticsearch.new(client_config[Rails.env].merge(logger: Rails.logger, create: client_config[:create]))
+    client = Search::Client::Elasticsearch.new(client_config.merge(logger: Rails.logger))
   elsif client_name.to_sym == :solr
-    client = Search::Client::Solr.new(client_config[Rails.env])
+    client = Search::Client::Solr.new(client_config)
   else
     raise "Invalid search client configured #{client_name}"
   end

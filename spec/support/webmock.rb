aaa34ab2c6ffd8e77dcb836b6a065fdf51620e4f
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-06-08 06:52:51 -0700

    Allow connections to localhost for JS integrations specs.

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,1 +1,7 @@
 require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.before(:each, :type => :request, :js => true) do
+    WebMock.disable_net_connect!(:allow_localhost => true)
+  end
+end

3b4ec3d3327e9612d3805e1ed18d9d1168b9a168
Kushal Pisavadia <kushi.p@gmail.com>
2012-07-09 07:11:44 -0700

    Improve readability of model

--- app/models/licence_link.rb
+++ app/models/licence_link.rb
@@ -9,16 +9,13 @@ class LicenceLink
   validates :activity_id, :presence => true
   validates :licence_id, :presence => true
 
-  index(
-      [
-          [ :sector_id, Mongo::ASCENDING ],
-          [ :activity_id, Mongo::ASCENDING ],
-          [ :licence_id, Mongo::ASCENDING ]
-      ],
-      :unique => true
-  )
+  index([[:sector_id, Mongo::ASCENDING],
+         [:activity_id, Mongo::ASCENDING],
+         [:licence_id, Mongo::ASCENDING]],
+        :unique => true)
 
   def self.find_by_sectors_and_activities(sectors, activities)
-    where(:sector_id.in => sectors.map(&:id), :activity_id.in => activities.map(&:id))
+    where(:sector_id.in => sectors.map(&:id),
+          :activity_id.in => activities.map(&:id))
   end
 end

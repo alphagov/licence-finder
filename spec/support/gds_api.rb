d92e4ea71acae4ae0fbedbe249d2fcf38b83cff5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-08-29 09:37:58 +0000

    Bump slimmer to 2.0.0 and pass artefact
    This will set the wrapper class to display the directgov and
    businesslink logos

--- spec/support/gds_api.rb
+++ spec/support/gds_api.rb
@@ -1,8 +1,16 @@
+require 'gds_api/test_helpers/panopticon'
 require 'gds_api/test_helpers/publisher'
 
 RSpec.configure do |config|
+  config.include GdsApi::TestHelpers::Panopticon, :type => :controller
+  config.before(:each, :type => :controller) do
+    stub_panopticon_default_artefact
+  end
+
+  config.include GdsApi::TestHelpers::Panopticon, :type => :request
   config.include GdsApi::TestHelpers::Publisher, :type => :request
   config.before(:each, :type => :request) do
+    stub_panopticon_default_artefact
     setup_publisher_licences_stubs
   end
 end

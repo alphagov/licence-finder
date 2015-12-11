ce81a033b12a0bad2367a8ffecbc0e281b1fba52
Tijmen Brommet <tijmen@gmail.com>
2015-12-11 16:50:46 +0000

    Test against v2 schemas
    This app was still sending old-style payload to the publishing-api.
    
    https://trello.com/c/catWOsWC

--- app/presenters/licence_finder_content_item_presenter.rb
+++ app/presenters/licence_finder_content_item_presenter.rb
@@ -16,11 +16,9 @@ class LicenceFinderContentItemPresenter
       base_path: base_path,
       title: metadata[:title],
       description: metadata[:description],
-      content_id: content_id,
       format: 'placeholder_licence_finder',
       publishing_app: 'licencefinder',
       rendering_app: 'licencefinder',
-      update_type: update_type,
       locale: 'en',
       public_updated_at: Time.now.iso8601,
       routes: [

970b5d3badf1ff241aff7f09aa7856ab3dc5b010
Murray Steele <muz@h-lame.com>
2016-10-03 12:51:39 +0000

    Enable webmock for all specs, not just request specs

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,7 @@
 require 'webmock/rspec'
 
 RSpec.configure do |config|
-  config.before(:each, type: :request) do
+  config.before(:each) do
     WebMock.disable_net_connect!(allow_localhost: true)
   end
 end

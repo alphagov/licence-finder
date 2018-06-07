754d34173a2a3ca25b0eb7325cf64fc3fda271ac
Tijmen Brommet <tijmen@gmail.com>
2018-06-07 08:33:27 +0100

    Remove usage of Slimmer::GovukComponents

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -5,6 +5,5 @@ RSpec.configure do |config|
     WebMock.disable_net_connect!(allow_localhost: true)
 
     stub_request(:get, Plek.new.find("content-store") + "/content/licence-finder").to_return(body: {}.to_json)
-    stub_request(:get, %r{#{Plek.new.find("static")}/templates/locales(.*)}).to_return(body: {}.to_json)
   end
 end

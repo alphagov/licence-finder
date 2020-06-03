alphagov/licence-finder

commit db8c489b2bae147774f79826fc6ea1d3b4b21818

Fix Style/GlobalVars for lib/search.rb
Previously an instance of this class was created as a global $search
variable, in several places, using a factory 'create' method. This
replaces the use of global vars by explicitly making this class a
Singleton, noting that we only ever use it in one environment at a time.

lib/search.rb
@@ -2,9 +2,12 @@ require "search/client/elasticsearch"
 require "erb"
 
 class Search
+  include Singleton
+
   attr_accessor :client
 
-  def self.create(environment = Rails.env)
+  def initialize
+    environment = Rails.env
     config_path = Rails.root + "config" + "elasticsearch.yml"
     client_config = HashWithIndifferentAccess.new(YAML.safe_load(ERB.new(File.read(config_path)).result))
     client_config = client_config[environment].merge(client_config[:all_envs])
@@ -16,7 +19,7 @@ class Search
       alias :write :info
     end
 
-    client = Search::Client::Elasticsearch.new(
+    @client = Search::Client::Elasticsearch.new(
       index_name: index_name,
       settings: settings,
       type: type,
@@ -25,26 +28,20 @@ class Search
         log: true,
       ),
     )
-
-    Search.new(client)
-  end
-
-  def initialize(client)
-    @client = client
   end
 
   def index_all
-    @client.pre_index
-    @client.index(Sector.find_layer3_sectors)
-    @client.post_index
+    client.pre_index
+    client.index(Sector.find_layer3_sectors)
+    client.post_index
   end
 
   def delete_index
-    @client.delete_index
+    client.delete_index
   end
 
   def search(query)
-    public_ids = @client.search(query)
+    public_ids = client.search(query)
 
     Sector.find_by_public_ids(public_ids).to_a.sort do |a, b|
       public_ids.index(a.public_id) <=> public_ids.index(b.public_id)

config/initializers/search.rb
@@ -1,3 +1,0 @@
-require "search"
-
-$search = Search.create

app/controllers/licence_finder_controller.rb
@@ -20,7 +20,7 @@ class LicenceFinderController < ApplicationController
   def sectors
     @picked_sectors = Sector.find_by_public_ids(extract_ids(:sector)).ascending(:name).to_a
     @sectors = if params[:q].present?
-                 $search.search(params[:q])
+                 Search.instance.search(params[:q])
                else
                  []
                end

lib/tasks/search.rake
@@ -4,6 +4,6 @@ task search: %w[search::index]
 namespace :search do
   desc "Index all Sectors"
   task index: :environment do
-    $search.index_all
+    Search.instance.index_all
   end
 end

spec/controllers/licence_finder_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe LicenceFinderController, type: :controller do
       @s2 = FactoryBot.create(:sector, name: "Charlie")
       @s3 = FactoryBot.create(:sector, name: "Bravo")
 
-      expect($search).to receive(:search).with("test query").and_return([@s1, @s2, @s3])
+      expect(Search.instance).to receive(:search).with("test query").and_return([@s1, @s2, @s3])
 
       get :sectors, params: { q: "test query" }
       expect(response).to be_successful
@@ -48,7 +48,7 @@ RSpec.describe LicenceFinderController, type: :controller do
     end
 
     it "returns slimmer headers" do
-      expect($search).to receive(:search).with("test query").and_return([])
+      expect(Search.instance).to receive(:search).with("test query").and_return([])
 
       get :sectors, params: { q: "test query" }
 
@@ -66,7 +66,7 @@ RSpec.describe LicenceFinderController, type: :controller do
       @s2 = FactoryBot.create(:sector, name: "Charlie")
       @s3 = FactoryBot.create(:sector, name: "Bravo")
 
-      expect($search).to receive(:search).with("test query").and_return([@s1, @s2, @s3])
+      expect(Search.instance).to receive(:search).with("test query").and_return([@s1, @s2, @s3])
 
       get :sectors, params: { q: "test query" }
       expect(response.headers["X-Slimmer-Result-Count"]).to eq("3")

spec/integration/finding_licences_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe "Finding licences", type: :request do
 
   specify "Simple happy path through the app" do
     WebMock.allow_net_connect!
-    $search = Search.create
 
     s1 = FactoryBot.create(:sector, name: "Fooey Sector", layer: 3)
     s2 = FactoryBot.create(:sector, name: "Kablooey Sector", layer: 3)
@@ -28,7 +27,7 @@ RSpec.describe "Finding licences", type: :request do
     FactoryBot.create(:licence_link, sector: s2, activity: a1, licence: l3)
     FactoryBot.create(:licence_link, sector: s1, activity: a1, licence: l4)
 
-    $search.index_all
+    Search.instance.index_all
     rummager_has_licences([], when_searching_for: [l1])
 
     visit "/#{APP_SLUG}/sectors"
@@ -118,6 +117,6 @@ RSpec.describe "Finding licences", type: :request do
       ])
     end
 
-    $search.delete_index
+    Search.instance.delete_index
   end
 end

spec/integration/sectors_page_spec.rb
@@ -3,17 +3,16 @@ require "rails_helper"
 RSpec.describe "Sector selection page", type: :request do
   before(:each) do
     WebMock.allow_net_connect!
-    $search = Search.create
 
     FactoryBot.create(:sector, public_id: 123, name: "Fooey Sector", layer: 3)
     FactoryBot.create(:sector, public_id: 234, name: "Kablooey Sector", layer: 3)
     FactoryBot.create(:sector, public_id: 345, name: "Gooey Sector", layer: 3)
 
-    $search.index_all
+    Search.instance.index_all
   end
 
   after(:each) do
-    $search.delete_index
+    Search.instance.delete_index
   end
 
   specify "inspecting the page" do

spec/lib/search/integration/elasticsearch_spec.rb
@@ -4,7 +4,6 @@ require "search/client/elasticsearch"
 RSpec.describe Search::Client::Elasticsearch do
   before(:each) do
     WebMock.allow_net_connect!
-    $search = Search.create
 
     FactoryBot.create(:sector, public_id: 123, correlation_id: 987, name: "Fooey Sector", layer: 3)
     s2 = FactoryBot.create(:sector, public_id: 234, correlation_id: 986, name: "Kablooey Sector", layer: 3)
@@ -13,37 +12,36 @@ RSpec.describe Search::Client::Elasticsearch do
     FactoryBot.create(:activity, public_id: 7123, correlation_id: 983, name: "Fooey activity", sectors: [s2])
     FactoryBot.create(:activity, public_id: 7124, correlation_id: 982, name: "Unrelated terms", sectors: [s3])
 
-    @search = $search.clone
-    allow(@search.client).to receive(:extra_terms).and_return(
+    allow(Search.instance.client).to receive(:extra_terms).and_return(
       987 => %w[foo bar],
       986 => %w[monkey],
     )
-    @search.index_all
+    Search.instance.index_all
   end
 
   after(:each) do
-    @search.delete_index
+    Search.instance.delete_index
   end
 
   it "returns sectors that match on title" do
-    expect(@search.search("fooey")[1].public_id).to eq(123)
-    expect(@search.search("sector").length).to eq(3)
+    expect(Search.instance.search("fooey")[1].public_id).to eq(123)
+    expect(Search.instance.search("sector").length).to eq(3)
   end
 
   it "returns sectors that match on extra terms" do
-    search = @search.search("monkey")
+    search = Search.instance.search("monkey")
     expect(search[0].public_id).to eq(234)
     expect(search.length).to eq(1)
   end
 
   it "returns sectors that match on activities" do
-    search = @search.search("unrelated")
+    search = Search.instance.search("unrelated")
     expect(search[0].public_id).to eq(345)
     expect(search.length).to eq(1)
   end
 
   it "returns sectors above activities when both match" do
-    search = @search.search("fooey")
+    search = Search.instance.search("fooey")
     expect(search.length).to eq(2)
     expect(search[1].public_id).to eq(123)
   end

commit 1cc2580ad1140d0fe5424376d13de95b5611114c

Eager and auto load files in lib directory
This is done off the back of a short incident that occurred today where
the application started erroring due to a constant (Search) being
unavailable.

This was caused by https://github.com/alphagov/licence-finder/commit/db8c489b2bae147774f79826fc6ea1d3b4b21818
where search used to be set-up by a strange global variable via an
initializer file. Once this was changed search was no longer available
unless the file was required (as was the case in the test environment)
and would error.

To simplify the differences between test and production I've set up all
of the lib files to be eager and auto loaded so that the
dev/test/production files are the same. As part of this I've removed the
various require statements as these are no longer needed.

app/models/activity.rb
@@ -1,5 +1,3 @@
-require "public_id"
-
 class Activity
   include Mongoid::Document
   include PublicId

app/models/sector.rb
@@ -1,5 +1,3 @@
-require "public_id"
-
 class Sector
   include Mongoid::Document
   include PublicId

config/application.rb
@@ -31,6 +31,9 @@ module LicenceFinder
 
     config.assets.prefix = "/assets/licencefinder"
 
+    config.eager_load_paths << Rails.root.join("lib")
+    config.autoload_paths << Rails.root.join("lib")
+
     # allow overriding the asset host with an enironment variable, useful for
     # when router is proxying to this app but asset proxying isn't set up.
     config.asset_host = ENV["ASSET_HOST"]

lib/search.rb
@@ -1,4 +1,3 @@
-require "search/client/elasticsearch"
 require "erb"
 
 class Search

lib/search/client/elasticsearch.rb
@@ -1,7 +1,3 @@
-require "search/client"
-require "search/client/search_result"
-require "elasticsearch"
-
 class Search
   class Client
     class Elasticsearch < Search::Client

lib/tasks/data_import.rake
@@ -1,6 +1,3 @@
-require "data_importer"
-require "licence_data_migrator"
-
 desc "Import all data from the CSV files"
 task data_import: ["data_import:sectors", "data_import:activities", "data_import:licences"]
 

lib/tasks/licence_migrate.rake
@@ -1,5 +1,3 @@
-require "licence_data_migrator"
-
 desc "Import all data from the CSV files"
 task licence_migrate: ["licence_migrate:all"]
 
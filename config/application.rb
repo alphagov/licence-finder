439265cc43398f4b01a06ab71d7525ff865db60d
Celia Collins <celiadcollins@gmail.com>
2020-07-14 12:23:42 +0100

    Run 'rails app:update'

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative "boot"
 
+require "rails"
 # Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
@@ -16,18 +17,13 @@ APP_SLUG = "licence-finder".freeze
 
 module LicenceFinder
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.assets.prefix = "/assets/licencefinder"
 

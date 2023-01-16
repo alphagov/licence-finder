168707809a64791d973937ef71ea01aa364fd9f4
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-01-16 17:08:51 +0000

    Export devolved administrations
    Exports the devolved administration data for each licence

--- app/lib/data_exporter.rb
+++ app/lib/data_exporter.rb
@@ -8,7 +8,20 @@ class DataExporter
     licences.map do |licence|
       {
         licence_identifier: licence.gds_id,
+        locations: locations(licence),
       }
     end
+  end
+
+private
+
+  def locations(licence)
+    locations = []
+    locations << "england" if licence.da_england
+    locations << "wales" if licence.da_wales
+    locations << "scotland" if licence.da_scotland
+    locations << "northern-ireland" if licence.da_northern_ireland
+
+    locations
   end
 end
--- spec/lib/data_exporter_spec.rb
+++ spec/lib/data_exporter_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe DataExporter do
 
       expect(exported_licences.first[:licence_identifier]).to eq(licence.gds_id)
     end
+
+    it "exports the applicable devolved administrations for each licence" do
+      exported_licences = DataExporter.call
+
+      expect(exported_licences.first[:locations]).to include("england")
+      expect(exported_licences.first[:locations]).to_not include("wales")
+      expect(exported_licences.first[:locations]).to_not include("scotland")
+      expect(exported_licences.first[:locations]).to_not include("northern-ireland")
+    end
   end
 end
